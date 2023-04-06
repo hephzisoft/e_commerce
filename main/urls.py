03fd521a506e28bda19e1417a48e2cae97c77741
Hephzisoft <hephzisoft@gmail.com>
2023-04-06 16:33:02 +0100

    Finished the add to cart functionality

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import home,cart,product_details,update_cart
+from .views import home, cart, product_details, update_cart, delete_item, cart_get_qty,get_qty
 
-urlpatterns=[
+urlpatterns = [
     path("",  home, name="home"),
-    path("product-details/<uuid:pk>/",product_details, name="product-details" ),
+    path("product-details/<uuid:pk>/", product_details, name="product-details"),
     path("cart/", cart, name="cart"),
 
-    # Add to cart  functionality 
-    path("update-cart", update_cart, name="update-cart"),
-]
+    # Add to cart  functionality
+    path("get-qty/<uuid:pk>/", get_qty, name="get-qty"),
+    path('update-cart/', update_cart, name="update-cart"),
+    path('delete-item/<uuid:pk>/', delete_item, name="delete-item"),
+    path("cart-get-qty/<uuid:pk>/", cart_get_qty, name="cart-get-qty"),]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.db.models import Q
 import json
-
+from django.middleware import csrf
 from .models import Product, Order, OrderItem
 from user.models import User
 
@@ -27,38 +27,130 @@ def product_details(request, pk):
 
 def cart(request):
     orderitem = OrderItem.objects.all()
-    context = {
-        'orderitem': orderitem
-    }
-    return render(request, 'main/cart.html',)
+    order, created = Order.objects.get_or_create(
+        complete=False, customer=request.user)
+
+    context = {"orderitem": orderitem, 'csrf_token': csrf.get_token(request)}
+    return render(request, 'main/cart.html', context)
 
 
 def update_cart(request):
     if request.method == 'POST':
-        data = json.dumps(request.body)
+        data = json.loads(request.body)
         action = data['action']
         product_id = data['id']
-        print(action)
-        print(product_id)
+
         product = Product.objects.get(id=product_id)
         order, created = Order.objects.get_or_create(
-            customer=request.user, complete=False)
-        orderitem = OrderItem.objects.get_or_create(
+            complete=False, customer=request.user)
+        orderitem, created = OrderItem.objects.get_or_create(
             product=product, order=order)
 
-        if action == "add" and orderitem.quantity >= 0:
-            orderitem.quantity = orderitem.quantity + 1
-        elif action == "add" and orderitem.quantity > 0:
+        if orderitem.quantity <= 0:
+            orderitem.delete()
+
+        if action == 'remove' and orderitem.quantity > 1:
             orderitem.quantity = orderitem.quantity - 1
+        elif action == "add" and orderitem.quantity >= 0:
+            orderitem.quantity = orderitem.quantity + 1
+        orderitem.save()
 
-            pass
-        context = {
-            'message': 'Item updated',
-            'qty': orderitem.quantity
-        }
-        return JsonResponse(context)
+        orders_total = ''
+        order_total = 0
+        for item in order.orderitem_set.all():
+            items_total = item.quantity * item.product.price
+            order_total += items_total
+            orders_total = str(order_total)
+        item_total = str(product.price * orderitem.quantity)
 
-    elif request.method == "GET":
-        context ={
-            'qty': orderitem.quantity
+        response_data = {
+            'qty': orderitem.quantity,
+            'item_total': item_total,
+            'orders_total': orders_total
+
         }
+
+    elif request.method == 'GET':
+        counts = OrderItem.objects.all().count()
+        response_data = {
+            'counts': counts,
+        }
+
+    return HttpResponse(
+        json.dumps(response_data),
+        content_type="application/json"
+    )
+
+
+def cart_get_qty(request, pk):
+    product = Product.objects.get(id=pk)
+    order, created = Order.objects.get_or_create(
+        complete=False, customer=request.user)
+    orderitem = OrderItem.objects.filter(
+        product=product, order=order).exists()
+
+    orders_total = ''
+    order_total = 0
+    for item in order.orderitem_set.all():
+        items_total = item.quantity * item.product.price
+        order_total += items_total
+        orders_total = str(order_total)
+
+    if orderitem:
+        cart_item = OrderItem.objects.get(product=product, order=order)
+        response = {
+            'qty': cart_item.quantity,
+            'item_total': str(items_total),
+            'orders_total': orders_total,
+
+        }
+    else:
+        orderitem.delete()
+        response = {
+            'orders_total': orders_total,
+        }
+
+    return HttpResponse(
+        json.dumps(response),
+        content_type="application/json"
+    )
+
+
+def get_qty(request, pk):
+
+    product = Product.objects.get(id=pk)
+    order, created = Order.objects.get_or_create(
+        complete=False, customer=request.user)
+    orderitem = OrderItem.objects.filter(
+        product=product, order=order).exists()
+
+    if orderitem:
+        cart_item = OrderItem.objects.get(order=order, product=product)
+        response = {
+            'qty': cart_item.quantity,
+        }
+    else:
+        response = {
+            'qty': 0
+        }
+
+    return HttpResponse(
+        json.dumps(response),
+        content_type="application/json"
+    )
+
+
+def delete_item(request, pk):
+    product = Product.objects.get(id=pk)
+    order, created = Order.objects.get_or_create(
+        complete=False, customer=request.user)
+    order_item = OrderItem.objects.get(order=order, product=product)
+    counts = OrderItem.objects.all().count()
+    order_item.delete()
+    orders_total = ''
+    order_total = 0
+    for item in order.orderitem_set.all():
+        items_total = item.quantity * item.product.price
+        order_total += items_total
+        orders_total = str(order_total)
+    return JsonResponse({'orders_total': orders_total, 'counts': counts})
